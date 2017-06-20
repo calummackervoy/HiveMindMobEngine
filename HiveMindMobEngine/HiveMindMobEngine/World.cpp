c9e9b27a217aafc84eb136e8102d17971a540a36
Calum Mackervoy <c.mackervoy@gmail.com>
2017-06-20 20:08:14 +0100

    Date & Time in World

--- HiveMindMobEngine/HiveMindMobEngine/World.cpp
+++ HiveMindMobEngine/HiveMindMobEngine/World.cpp
@@ -31,3 +31,53 @@ void World::removeTech(int i) {
     if(i < 0 || i >= MAX_TECHS) return;
     techs[i].id = -1;
 }
+
+void World::addMonth() {
+    if(dateTime.month == 12) {
+        addYear();
+        dateTime.month = 1;
+    }
+    else dateTime.month++;
+}
+
+void World::addDay() {
+    switch(dateTime.month) {
+    //months with 31
+    case 1: case 3: case 5: case 7:
+    case 8: case 10: case 12:
+        if(dateTime.day == 31) {
+            addMonth();
+            dateTime.day = 1;
+        }
+        else dateTime.day++;
+        break;
+    //months with 30
+    case 4: case 6: case 9: case 11:
+        if(dateTime.day == 30) {
+            addMonth();
+            dateTime.day = 1;
+        }
+        else dateTime.day++;
+        break;
+    //and February
+    case 2:
+        //is it leap year?
+        bool leapYear = ((dateTime.year % 4) == 0);
+        if((!leapYear && dateTime.day == 28) ||
+            (leapYear && dateTime.day == 29)) {
+            addMonth();
+            dateTime.day = 1;
+        }
+        else dateTime.day++;
+        break;
+    }
+    
+}
+
+void World::addHour() {
+    if(dateTime.hour == 24) {
+        addDay();
+        dateTime.hour = 1;
+    }
+    else dateTime.hour++;
+}
--- HiveMindMobEngine/HiveMindMobEngine/World.h
+++ HiveMindMobEngine/HiveMindMobEngine/World.h
@@ -3,9 +3,17 @@
 #include "Technology.h"
 #include "FileHandler.h"
 #include "Config.h"
+#include "TypeDef.h"
 
 //TODO: replace all const ints with uint8_t where necessary
 const int MAX_REGIONS = 32;
+
+struct DateTime {
+	suint year;
+	uint8_t month;
+	uint8_t day;
+	uint8_t hour;
+};
 
 class World {
 public:
@@ -18,7 +26,20 @@ public:
 	inline int addTech(Tech* t);
 	inline void removeTech(int i);
 
+	//Date/Time getters and setters
+	inline DateTime getDateTime() {return dateTime;};
+	inline suint getYear() {return dateTime.year;};
+	inline uint8_t getMonth() {return dateTime.month;};
+	inline uint8_t getDay() {return dateTime.day;};
+	inline uint8_t getHour() {return dateTime.hour;};
+	inline void setDateTime(DateTime val) {dateTime = val;};
+	inline void addYear() {	dateTime.year++; };
+	inline void addMonth();
+	inline void addDay();
+	inline void addHour();
+
 protected:
 	Region* regions[MAX_REGIONS];
 	Tech* techs[MAX_TECHS];
+	DateTime dateTime;
 };
