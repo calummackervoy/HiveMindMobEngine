b8d577daa4affb80ad2488712d75bfd47b0a8535
Calum Mackervoy <c.mackervoy@gmail.com>
2017-06-29 04:08:10 +0100

    Changes to Terrain.h -Plus Freerun.h containing free-running options of a Tile

---
+++ HiveMindMobEngine/HiveMindMobEngine/Freerun.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <cstdint>
+
+enum FreeRunTypes : uint8_t {
+	CLIMB_UPWARD,
+	RUN_CLIMB_UPWARD,
+	JUMP
+};
--- HiveMindMobEngine/HiveMindMobEngine/Terrain.h
+++ HiveMindMobEngine/HiveMindMobEngine/Terrain.h
@@ -1,18 +1,75 @@
 #pragma once
+#include <SFML\Graphics.hpp>
 #include "Typedef.h"
+#include "Freerun.h"
 
 const uint8_t MAX_WIDTH = 1000;
 const uint8_t MAX_HEIGHT = 1000;
+const uint8_t NUM_TERRAIN_TYPES = 8;
 
 enum Terrain : uint8_t {
-	OPEN,
-	PART_COV,
-	FULL_COV
+	FLAT_GROUND,
+	SLIGHT_INCLINE,
+	DRASTIC_INCLINE,
+	WALL,
+	DOORWAY,
+	WINDOW,
+	PROP,
+	INTERACTIVE
 };
 
-//TODO: children with data for holding things useful to RegionMaps, BattleMaps and WorldMaps
+enum TerrainGraphic : uint8_t {
+	DEFAULT = 0
+};
+
+const sf::Color terrainColours[NUM_TERRAIN_TYPES] = {
+
+};
+
 struct Tile {
-	Terrain terrain = OPEN;
+	TerrainGraphic terrainGraphic = DEFAULT;
+	Terrain terrain = FLAT_GROUND;
+	uint8_t level = 0;
+	bool freeRunnable = false;
+};
+
+enum Climate : uint8_t {
+	ARCTIC,
+	TUNDRA,
+	TEMPERATE_COLD,
+	TEMPERATE_WARM,
+	PLAINS,
+	TROPICAL,
+	ARID,
+	DESERT
+};
+
+enum FeaturePresence : uint8_t {
+	EVERYWHERE,
+	ABUNDANT,
+	LARGE_AREA,
+	SPARSE,
+	SMALL_AREA,
+	SINGULAR,
+	NO_PRESENCE
+};
+
+struct RegionTile {
+	Climate climate = TEMPERATE_COLD;
+	FeaturePresence mountains = NO_PRESENCE;
+	FeaturePresence urban = LARGE_AREA;
+	FeaturePresence rural = SPARSE;
+	FeaturePresence woodland = SPARSE;
+	FeaturePresence swamp = NO_PRESENCE;
+	FeaturePresence water = NO_PRESENCE;
+	//FLAG: 6 because hectagons being used for tiles, you may want to change
+	bool coastal[6] = { false, false, false, false, false, false };
+
+	//for water tiles
+	bool isWaterTile = false;
+	FeaturePresence underwaterCity = NO_PRESENCE;
+	FeaturePresence floatingCity = NO_PRESENCE;
+	FeaturePresence coralReef = NO_PRESENCE;
 };
 
 typedef struct Grid {
