8fd37bbcaffaeea237486beb7c22dfec11eee389
Calum Mackervoy <c.mackervoy@gmail.com>
2017-07-23 12:50:20 +0100

    Interface detection in Physics subsystem

--- HiveMindMobEngine/HiveMindMobEngine/Physics.cpp
+++ HiveMindMobEngine/HiveMindMobEngine/Physics.cpp
@@ -1,1 +1,21 @@
 #include "Physics.h"
+
+Physics::Physics() {
+
+}
+
+Physics::~Physics() {
+
+}
+
+//interface detection with any BoundingBox
+bool Physics::interfaceBoundingBox(sf::Vector2i pos, BoundingBox area) {
+	float maxx = pos.x + area.width;
+	float maxy = pos.y + area.height;
+
+	if (pos.x <= maxx && pos.x >= area.x &&
+		pos.y <= maxy && pos.y >= area.y) {
+		return true;
+	}
+	return false;
+}
--- HiveMindMobEngine/HiveMindMobEngine/Physics.h
+++ HiveMindMobEngine/HiveMindMobEngine/Physics.h
@@ -1,5 +1,24 @@
 #pragma once
+#include "RendererConfig.h"
+
+struct BoundingBox {
+	int height;
+	int width;
+	//note: position is position of bottom-left of square
+	int x;
+	int y;
+};
 
 class Physics {
+public:
+	Physics();
+	~Physics();
+
+	//interface detection with any BoundingBox
+	bool interfaceBoundingBox(sf::Vector2i pos, BoundingBox area);
+
+	//TODO: ray casting
+
+private:
 
 };
