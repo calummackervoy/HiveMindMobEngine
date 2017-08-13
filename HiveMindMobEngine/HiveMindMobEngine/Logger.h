ef14c038dd923e19cf8ca6a1a968c42eb105c67e
Calum Mackervoy <c.mackervoy@gmail.com>
2017-08-13 22:55:23 +0100

    Logger provides convenience functions for logging to console -Also asset for the default background texture

---
+++ HiveMindMobEngine/HiveMindMobEngine/Logger.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <string>
+#include <iostream>
+
+//Author: Calum Mackervoy
+//Purpose: convenience functions for different kinds of messages to the console
+//TODO: add support for logging to file
+
+using std::cout;
+using std::endl;
+using std::string;
+
+class Logger {
+public:
+	inline static void logMessage(string message) {
+		cout << message << endl;
+	};
+	inline static void logError(string source, string error) {
+		cout << "[ERR-" << source << "] " << error << endl;
+	}
+};
