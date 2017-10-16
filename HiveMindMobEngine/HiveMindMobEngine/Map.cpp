64ab66873d13c6b224f5b06b2d745eba6d4359ca
Calum Mackervoy <c.mackervoy@gmail.com>
2017-10-16 23:08:51 +0100

    Map stores an array of Tiles

--- HiveMindMobEngine/HiveMindMobEngine/Map.cpp
+++ HiveMindMobEngine/HiveMindMobEngine/Map.cpp
@@ -1,15 +1,22 @@
 #include "Map.h"
 
-Map::Map(suint size) {
+Map::Map(const suint size) {
 	//mapType = MAP_WORLD;
-	map = NULL;
-	this->size = size;
+	//enforce maximum map size
+	this->size = std::max((uint)size, MAX_MAP_SIZE);
+
+	//initialise map to array of NULLs
+	for (int i = 0; i < this->size; i++) {
+		for (int j = 0; j < this->size; j++) {
+			map[i][j] = NULL;
+		}
+	}
 }
 
 Map::~Map() {
 	delete map;
 }
 
-sf::Vector2i Map::convertToTile(sf::Vector2f pos) {
+sf::Vector2i Map::convertToTile(const sf::Vector2f pos) {
 	return sf::Vector2i(floor(pos.x / TILE_SIZE), floor(pos.y / TILE_SIZE));
 }
--- HiveMindMobEngine/HiveMindMobEngine/Map.h
+++ HiveMindMobEngine/HiveMindMobEngine/Map.h
@@ -11,27 +11,26 @@
 
 using std::string;
 
+//NOTE: map tiles are organised in 2D array like so: map[x][y]
 class Map {
 public:
-	Map(suint size);
+	Map(const suint size);
 	~Map();
 
-	/*inline Tile getTerrainAt(int x, int y) {
-		if (mapType == MAP_WORLD) {
-			return ((WorldGrid*)(map))->grid[x][y];
-		}
-		else {
-
-		}
-	};*/
+	inline Tile* getTerrainAt(int x, int y) {
+		//bounds checking
+		if (x < 0 || x > size || y < 0 || y > size) return NULL;
+		return map[x][y];
+	};
 
 	//setting a new map
 	//void setMap(string mapLocation) { map = FileHandler::readMap(mapLocation); };
+
 	//method to get which tile a given x,y co-ordinate is in
-	sf::Vector2i convertToTile(sf::Vector2f pos);
+	sf::Vector2i convertToTile(const sf::Vector2f pos);
 
 protected:
 	//MapType mapType;
-	Grid* map;
+	Tile* map[MAX_MAP_SIZE][MAX_MAP_SIZE];
 	suint size;
 };
